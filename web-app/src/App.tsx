c7204a36a344090aba65d2245ccccac716f3b6b4
Brett Sodie <blsodie@gmail.com>
2025-01-27 20:14:35 -0500

    feat: add react app

---
+++ web-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from '@mui/joy/Button';
+
+
+function App() {
+
+  return (
+    <>
+      Finance
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
