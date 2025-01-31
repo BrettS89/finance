e5208d8bbde6ad441d4523a4bdff839963dd2682
Brett Sodie <blsodie@gmail.com>
2025-01-30 19:56:19 -0500

    feat: add patch response

--- api/src/services/expense-type/controllers.ts
+++ api/src/services/expense-type/controllers.ts
@@ -20,6 +20,7 @@ router.get('/expense-type', async (req, res) => {
 
 router.patch('/expense-type/:id', async (req, res) => {
   const result = await new ExpenseTypeService().patch(req.params.id, req.body);
+  res.status(200).json(result);
 });
 
 router.delete('/expense-type/:id', async (req, res) => {
