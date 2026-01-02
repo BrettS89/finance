783ccfed5a478358348f2ab57b70b2c1d988d729
Brett Sodie <blsodie@gmail.com>
2026-01-02 13:30:12 -0500

    fix: update import

--- api-v2/src/tracing.ts
+++ api-v2/src/tracing.ts
@@ -1,8 +1,8 @@
-import opentelemetry from '@opentelemetry/sdk-node';
+import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter(),
     instrumentations: [
         getNodeAutoInstrumentations({
